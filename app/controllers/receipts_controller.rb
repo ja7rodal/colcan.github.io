48cd3fad55dbdf27a42a645676c623472773a240
Jasson Rdz <ja7rodal@gmail.com>
2017-02-20 10:31:05 -0500

    views per receipts

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,2 +1,46 @@
 class ReceiptsController < ApplicationController
+		
+	before_action :authenticate_user!
+	
+	def index
+		@receipts = Receipt.take(60)
+	end
+
+	def new
+		@receipt = Receipt.new
+	end
+
+	def create
+		@receipt = Receipt.create(receipt_params)
+		if @receipt.save
+			redirect_to receipts_path, notice: "Receipt successfully published"
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@receipt = Receipt.find(params[:id])
+	end
+
+	def update
+		@receipt = Receipt.find(params[:id])
+		if @receipt.update(receipt_params)
+			redirect_to receipts_path, notice: "Current Receipt"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		receipt = Receipt.find(params[:id])
+		receipt.destroy
+
+		redirect_to receipts_path, notice: "Receipt delete successfully"
+	end
+
+	private
+	def receipt_params
+		params.require(:receipt).permit(:amount, :temperature)
+	end
 end
